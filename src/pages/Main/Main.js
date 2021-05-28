df509310259412d9a4a6251e52c8529f23248312
Tal Gelfand <talj.gelfand@gmail.com>
2021-05-28 23:09:45 +0300

    add title on the main page

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,10 +2,13 @@ import React from "react";
 import CoursesGrid from "../../components/CoursesGrid";
 import Navbar from "../../components/Navbar";
 
+import "./Main.scss";
+
 const Main = () => {
   return (
     <section className="main">
       <Navbar />
+      <h1 className="main__title">Our best courses</h1>
       <CoursesGrid />
     </section>
   );
