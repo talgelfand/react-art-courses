86cf69ebf7cb77d983be673cf7877a8294f96e9a
Tal Gelfand <talj.gelfand@gmail.com>
2021-05-31 14:54:41 +0300

    fetch people (teachers) from api

--- src/pages/Teachers/Teachers.js
+++ src/pages/Teachers/Teachers.js
@@ -1,55 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import "./Teachers.scss";
 
 const Teachers = () => {
+  const [teachers, setTeachers] = useState([]);
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+  const fetchData = () => {
+    axios.get(url).then((response) => {
+      setTeachers(response.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const teachersList = teachers.map((teacher) => {
+    return (
+      <li key={teacher.id} className="teachers__item">
+        <h3 className="teachers__item-title">{teacher.name}</h3>
+        <p className="teachers__item-descr">
+          {teacher.email}
+          <br />
+          {teacher.company.catchPhrase}
+        </p>
+      </li>
+    );
+  });
+
   return (
     <section className="teachers">
       <h1 className="teachers__title">Our teachers</h1>
-      <h2 className="teachers__subtitle">
-        Here will be the list of teachers rendered from teachers.json
-      </h2>
-      <ul className="teachers__list">
-        <li className="teachers__item">
-          <h3 className="teachers__item-title">John Doe</h3>
-          <p className="teachers__item-descr">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-            repellendus deleniti cupiditate autem quisquam id vel reprehenderit
-            maiores quam, tempora, unde quia vero eius sequi excepturi placeat
-            dolores a nisi?
-          </p>
-        </li>
-        <li className="teachers__item">
-          {" "}
-          <h3 className="teachers__item-title">John Doe</h3>
-          <p className="teachers__item-descr">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-            repellendus deleniti cupiditate autem quisquam id vel reprehenderit
-            maiores quam, tempora, unde quia vero eius sequi excepturi placeat
-            dolores a nisi?
-          </p>
-        </li>
-        <li className="teachers__item">
-          {" "}
-          <h3 className="teachers__item-title">John Doe</h3>
-          <p className="teachers__item-descr">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-            repellendus deleniti cupiditate autem quisquam id vel reprehenderit
-            maiores quam, tempora, unde quia vero eius sequi excepturi placeat
-            dolores a nisi?
-          </p>
-        </li>
-        <li className="teachers__item">
-          {" "}
-          <h3 className="teachers__item-title">John Doe</h3>
-          <p className="teachers__item-descr">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
-            repellendus deleniti cupiditate autem quisquam id vel reprehenderit
-            maiores quam, tempora, unde quia vero eius sequi excepturi placeat
-            dolores a nisi?
-          </p>
-        </li>
-      </ul>
+      <ul className="teachers__list">{teachersList}</ul>
     </section>
   );
 };
