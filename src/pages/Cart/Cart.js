47d13e799e11fcf9864c8ebdabc6ea9f70ec5112
Tal Gelfand <talj.gelfand@gmail.com>
2021-06-02 12:22:39 +0300

    count total price in cart

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -21,12 +21,42 @@ const StyledButton = styled(Button)`
   font-size: 22px;
 `;
 
+const TotalPrice = styled.h2`
+  text-align: center;
+  margin-top: 20px;
+  font-size: 20px;
+`;
+
+const Checkout = styled(Button)`
+  display: block;
+  margin: 0 auto;
+  margin-top: 20px;
+  background-color: var(--accent-color);
+  border-color: var(--accent-color);
+  &:hover {
+    background-color: var(--primary-color);
+    border-color: var(--primary-color);
+  }
+`;
+
 const Cart = () => {
   const { cartItems, setCartItems } = useContext(Context);
+  let totalPrice = 0;
 
   const clearAllCourses = () => {
     setCartItems([]);
   };
+
+  const countTotalPrice = () => {
+    cartItems.forEach((item) => {
+      totalPrice += item.priceNum;
+    });
+
+    return totalPrice;
+  };
+
+  console.table(cartItems);
+  console.log(totalPrice);
 
   const courses = cartItems.map((item) => {
     return <CartItem key={item.id} {...item} />;
@@ -42,6 +72,8 @@ const Cart = () => {
       <StyledButton color="link" onClick={clearAllCourses}>
         Clear all
       </StyledButton>
+      <TotalPrice>{`Total: ${countTotalPrice()} euros`}</TotalPrice>
+      <Checkout>Go to checkout</Checkout>
     </Section>
   );
 };
