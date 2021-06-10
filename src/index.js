1dd2c25b25a12d079035cf2b1c382f2b766f2b29
Tal Gelfand <talj.gelfand@gmail.com>
2021-06-10 12:45:40 +0300

    move firebase config to .env.local

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import { ContextProvider } from "./context/context";
 import firebase from "firebase/app";
 import "firebase/auth";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyBUHU8gQwo7PuETlvPVq88Fg3T_7KMlgZM",
-  authDomain: "art-academy-af7d3.firebaseapp.com",
-  projectId: "art-academy-af7d3",
-  storageBucket: "art-academy-af7d3.appspot.com",
-  messagingSenderId: "592992154767",
-  appId: "1:592992154767:web:f571568632b94ca4a82cfa",
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
